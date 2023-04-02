3bac2d0c970ab3062c8353045c56c11df9114293
marianoajl <marianoajl@hotmail.com>
2023-04-02 01:36:55 -0300

    Cambio de fetch en JS con Axios + agregado de estilos en el CSS

--- app.js
+++ app.js
@@ -1,59 +1,81 @@
-const axios = require("axios");
+let apiKey = "ff066e2dc53fd040fca6669e93dab514";
 
 // funcion para que cargue las peliculas...
 window.addEventListener("load", () => {
-    cargarPeliculas();
+    getMovies();
 });
 
-let pagina = 1; // variable para controla la paginacion
+let page = 1; // variable para controla la paginacion
 
 //capturar los botones
 
-let btnAnterior = document.querySelector("#btnAnterior");
-let btnSiguiente = document.querySelector("#btnSiguiente");
+let btnAnterior = document.querySelector("#btn-previous");
+let btnSiguiente = document.querySelector("#btn-next");
 /* 
 funcion boton anterior */
 
-btnAnterior.addEventListener("click", () => {
-    if (pagina > 1) {
-        /*    pagina = pagina -1 */
-        pagina -= 1;
-        //llamar a la funcion que carga la pagina
-        cargarPeliculas();
-    }
-});
+// btnAnterior.addEventListener("click", () => {
+//     if (pagina > 1) {
+//         /*    pagina = pagina -1 */
+//         pagina -= 1;
+//         //llamar a la funcion que carga la pagina
+//         cargarPeliculas();
+//     }
+// });
 
-btnSiguiente.addEventListener("click", () => {
-    if (pagina <= 500) {
-        pagina += 1;
-        //llamar a la funcion que carga la pagina
-        cargarPeliculas();
-    }
-});
+// btnSiguiente.addEventListener("click", () => {
+//     if (pagina <= 500) {
+//         pagina += 1;
+//         //llamar a la funcion que carga la pagina
+//         cargarPeliculas();
+//     }
+// });
 
 // funcion que carga las peliculas
 
-const cargarPeliculas = async () => {
+const getMovies = async () => {
     try {
-        let respuesta = await fetch(
-            `https://api.themoviedb.org/3/movie/popular?api_key=TUAPIKEY&language=es-MX&page=${pagina}`
+        let resMovies = await axios(
+            `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-MX&page=${page}`
         );
-
-        if (respuesta.status === 200) {
-            let datos = await respuesta.json();
-            let peliculas = "";
-            datos.results.forEach((pelicula) => {
-                peliculas += `<div class="pelicula">
-<img class= "poster" src="https://image.tmdb.org/t/p/w500${pelicula.poster_path}"  />
-<h3 class= "titulo">${pelicula.title}</h3>
-</div>`;
+        if (resMovies.status === 200) {
+            console.log(resMovies);
+            let movies = "";
+            resMovies.data.results.forEach((movie) => {
+                movies += `<div id="${movie.id}" class="movie">
+                                <a class="movie-link" href=""></a>
+                                <img class="movie-img" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}">
+                                <h6 class="movie-title">${movie.original_title}</h6>
+                           </div>`;
             });
-            document.querySelector(".contenedor").innerHTML = peliculas;
-        } else if (respuesta.status === 404) {
-            console.log("la pagina no existe");
+            document.querySelector("#container").innerHTML = movies;
         }
     } catch (error) {
         console.log(error);
     }
-    document.querySelector(".pagina").innerHTML = `Pagina N: ${pagina}`;
 };
+
+// const cargarPeliculas = async () => {
+//     try {
+//         let respuesta = await fetch(
+//             `https://api.themoviedb.org/3/movie/popular?api_key=TUAPIKEY&language=es-MX&page=${pagina}`
+//         );
+
+//         if (respuesta.status === 200) {
+//             let datos = await respuesta.json();
+//             let peliculas = "";
+//             datos.results.forEach((pelicula) => {
+//                 peliculas += `<div class="pelicula">
+// <img class= "poster" src="https://image.tmdb.org/t/p/w500${pelicula.poster_path}"  />
+// <h3 class= "titulo">${pelicula.title}</h3>
+// </div>`;
+//             });
+//             document.querySelector(".contenedor").innerHTML = peliculas;
+//         } else if (respuesta.status === 404) {
+//             console.log("la pagina no existe");
+//         }
+//     } catch (error) {
+//         console.log(error);
+//     }
+//     document.querySelector(".pagina").innerHTML = `Pagina N: ${pagina}`;
+// };
